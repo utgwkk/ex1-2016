886d5ba7cdf369541175967c28d2118978fb506d
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-13 17:32:38 +0900

    標準入力の終わりまで取得する

--- kadai3/SortTest.java
+++ kadai3/SortTest.java
@@ -4,17 +4,19 @@ import java.io.*;
 public class SortTest {
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    int n = Integer.parseInt(scan.next());
     ArrayList<Integer> l = new ArrayList<Integer>();
-    for(int i=0;i<n;i++){
-      l.add(Integer.parseInt(scan.next()));
+    while(scan.hasNext()){
+      int inint = Integer.parseInt(scan.next());
+      if(inint > 0 && inint < 1000)
+        l.add(inint);
     }
     int[] a = l.stream().mapToInt(i->i).toArray();
     BubbleSort bs = new BubbleSort();
     a = bs.sort(a);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<a.length;i++){
       if(i%10==0&&i>0)System.out.println();
-      System.out.print(String.format("%1$4d ", a[i]));
+      System.out.print(String.format("%1$4d", a[i]));
+      if(i%10==9&&i!=a.length-1)System.out.print(" ");
     }
   }
 }
