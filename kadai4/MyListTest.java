688934d46ab0bc94f0caf5582eda9b9ecefb91c2
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-16 14:51:48 +0900

    リストの初期状態を出力

--- kadai4/MyListTest.java
+++ kadai4/MyListTest.java
@@ -5,12 +5,13 @@ public class MyListTest {
   public static void main (String[] args) {
     Scanner scan = new Scanner(System.in);
     ArrayList<Integer> l = new ArrayList<Integer>();
+    MyList mylist = new MyList();
     for (int i = 0; i < 10; i++) {
       int inint = Integer.parseInt(scan.next());
       if (inint > 0 && inint < 10000)
         l.add(inint);
     }
-    MyList mylist = new MyList();
+    mylist.output(l);
     for (int i = 0; i < 10; ++i) {
       int inint = Integer.parseInt(scan.next());
       mylist.insert(inint, l);
