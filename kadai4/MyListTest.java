1a16ee7b5ad08fac270437d2f60fb49a19e732e4
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-16 14:30:17 +0900

    MyList#delete まで追加

---
+++ kadai4/MyListTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class MyListTest {
+  public static void main (String[] args) {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> l = new ArrayList<Integer>();
+    while (scan.hasNext()) {
+      int inint = Integer.parseInt(scan.next());
+      if (inint > 0 && inint < 10000)
+        l.add(inint);
+    }
+    MyList mylist = new MyList();
+  }
+}
