9cb38a70bf147a164de1abe874da0c3bd7e8d6de
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-16 14:30:17 +0900

    MyList#delete まで追加

---
+++ kadai4/MyList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class MyList {
+  public void insert(int x, ArrayList<Integer> l) {
+  }
+
+  public void delete(int x, ArrayList<Integer> l) {
+    while (l.contains(x))
+      l.remove(x);
+  }
+
+  public void output(ArrayList<Integer> l) {
+  }
+}
