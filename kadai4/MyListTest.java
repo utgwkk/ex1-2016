9d7e17a84b0946f961a68569c684254916608f35
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-16 14:44:34 +0900

    10回入力

--- kadai4/MyListTest.java
+++ kadai4/MyListTest.java
@@ -5,7 +5,7 @@ public class MyListTest {
   public static void main (String[] args) {
     Scanner scan = new Scanner(System.in);
     ArrayList<Integer> l = new ArrayList<Integer>();
-    while (scan.hasNext()) {
+    for (int i = 0; i < 10; i++) {
       int inint = Integer.parseInt(scan.next());
       if (inint > 0 && inint < 10000)
         l.add(inint);
