d85d27e1edc9e875de8392b273ba76829ca296df
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-20 15:53:48 +0900

    課題10

---
+++ 10/MyEdge.java
@@ -0,0 +1,9 @@
+public class MyEdge {
+  public int from, to, dest;
+
+  public MyEdge (int f, int t, int d) {
+    this.from = f;
+    this.to = t;
+    this.dest = d;
+  }
+}
---
+++ 10/MyGraph.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MyGraph {
+  private ArrayList<MyEdge> graph;
+
+  public void ReadFromFile (String filename) {
+    File file = new File(filename);
+    Scanner scanner = new Scanner(file);
+    int v = scanner.nextInt();
+    int e = scanner.nextInt();
+  }
+
+  public LinkedList<MyEdge> getEdges (int id) {
+  }
+}
