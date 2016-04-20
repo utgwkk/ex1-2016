4fc0ead548179eae2960ecdae3bfffa243e0c384
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-20 13:13:59 +0900

    using Scanner.nextInt()

--- 4/MyListTest.java
+++ 4/MyListTest.java
@@ -7,18 +7,18 @@ public class MyListTest {
     ArrayList<Integer> l = new ArrayList<Integer>();
     MyList mylist = new MyList();
     for (int i = 0; i < 10; i++) {
-      int inint = Integer.parseInt(scan.next());
+      int inint = scan.nextInt();
       if (inint > 0 && inint < 10000)
         l.add(inint);
     }
     mylist.output(l);
     for (int i = 0; i < 10; ++i) {
-      int inint = Integer.parseInt(scan.next());
+      int inint = scan.nextInt();
       mylist.insert(inint, l);
       mylist.output(l);
     }
     for (int i = 0; i < 10; ++i) {
-      int inint = Integer.parseInt(scan.next());
+      int inint = scan.nextInt();
       mylist.delete(inint, l);
       mylist.output(l);
     }
