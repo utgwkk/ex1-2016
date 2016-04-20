368d1856e4ad37e814d56e5124adfd1a68e759b8
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-04-20 16:15:05 +0900

    課題10 done

--- 10/MyGraph.java
+++ 10/MyGraph.java
@@ -1,15 +1,35 @@
+import java.io.*;
 import java.util.*;
 
 public class MyGraph {
-  private ArrayList<MyEdge> graph;
+  private ArrayList<ArrayList<MyEdge>> graph;
 
   public void ReadFromFile (String filename) {
-    File file = new File(filename);
-    Scanner scanner = new Scanner(file);
-    int v = scanner.nextInt();
-    int e = scanner.nextInt();
+    try {
+      File file = new File(filename);
+      Scanner scanner = new Scanner(file);
+      int v = scanner.nextInt();
+      int e = scanner.nextInt();
+      graph = new ArrayList<ArrayList<MyEdge>>();
+      for (int i = 0; i < v; ++i)
+        graph.add(new ArrayList<MyEdge>());
+      for (int i = 0; i < e; ++i) {
+        int f,t,d;
+        f = scanner.nextInt();
+        t = scanner.nextInt();
+        d = scanner.nextInt();
+        graph.get(f).add(new MyEdge(t,d));
+      }
+    } catch (FileNotFoundException e) {
+    }
   }
 
-  public LinkedList<MyEdge> getEdges (int id) {
+  public ArrayList<MyEdge> getEdges (int id) {
+    ArrayList<MyEdge> result = new ArrayList<>();
+    for (int i = 0; i < this.graph.get(id).size(); ++i) {
+      // System.out.println(i);
+      result.add(this.graph.get(id).get(i));
+    }
+    return result;
   }
 }
