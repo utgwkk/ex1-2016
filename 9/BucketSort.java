afac0d008c373ca3a7152849fe53c5e69e2d1222
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-06-01 14:01:16 +0900

    課題9 バケットソート

---
+++ 9/BucketSort.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class BucketSort {
+  public ArrayList<String> sort (ArrayList<String> input) {
+    ArrayList<ArrayList<String>> bucket = new ArrayList<>(50);
+    return input;
+  }
+}
---
+++ 9/SortTest.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class SortTest {
+  public static void main (String[] args) {
+    ArrayList<String> input = read();
+    if (input == null) {
+      System.out.println("不正です");
+      return;
+    }
+    BucketSort bs = new BucketSort();
+    input = bs.sort(input);
+    for (int i = 0; i < input.size(); ++i) {
+      System.out.print(String.format("%s ", input.get(i)));
+    }
+    System.out.println();
+  }
+
+  public static ArrayList<String> read () {
+    System.out.print("inputファイル名を入力してください: ");
+    System.out.flush();
+    Scanner stdin = new Scanner(System.in);
+    String infilename = stdin.next();
+    ArrayList<String> l = new ArrayList<>();
+    try {
+      File inputFile = new File(infilename);
+      String content = new String(Files.readAllBytes(Paths.get(infilename)));
+      content = content.toLowerCase();
+      content = content.replaceAll(",", " ");
+      content = content.replaceAll("\\.", " ");
+      Scanner scan = new Scanner(content);
+      while (scan.hasNext()) {
+        String str = scan.next();
+        l.add(str);
+      }
+      return l;
+    } catch (FileNotFoundException e) {
+      System.out.println("ファイル " + infilename + " は存在しません");
+      return null;
+    } catch (IOException e) {
+      System.out.println("ファイル " + infilename + " の読み込みに失敗しました ");
+      return null;
+    }
+  }
+}
